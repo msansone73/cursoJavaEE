30777109f69f0d5cf2729ba644ec1667509ba7bf
marcio sansone <msansone@gmail.com>
2019-01-29 21:44:28 +0000

    Add Usuario relationship

--- hello-todo/src/main/java/br/com/msansone/hellotodo/entity/Todo.java
+++ hello-todo/src/main/java/br/com/msansone/hellotodo/entity/Todo.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.PrePersist;
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotEmpty;
@@ -33,8 +34,19 @@ public class Todo {
 	private LocalDate dateCompleted;
 	private LocalDate dateCreated;
 	
+	@ManyToOne
+	private Usuario usuario;
 	
 	
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	public Long getId() {
 		return id;
 	}
