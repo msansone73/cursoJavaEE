8b627f5181f97f3cf9ebc0df5c96ff2173ce2e03
marcio sansone <msansone@gmail.com>
2019-01-29 21:08:30 +0000

    Add Usuario

--- hello-todo/src/main/java/br/com/msansone/hellotodo/entity/AbstractEntity.java
+++ hello-todo/src/main/java/br/com/msansone/hellotodo/entity/AbstractEntity.java
@@ -1,6 +1,5 @@
 package br.com.msansone.hellotodo.entity;
 
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
